94a567008ea208953848a79e43456bae6e00d5b6
Cocktailrockstar <cocktailrockstar@gmail.com>
2017-05-21 11:44:05 +0200

    wrote two test for import functionality

---
+++ tests/testthat/testimport.R
@@ -0,0 +1,16 @@
+library("NeuroIS")
+context("Test of all Import Functions")
+
+test_that("Detected DataType is real DataType", {
+  expect_equal(detectDataType("datafortestingimport/ExpData_raw.csv"), "exp")
+  expect_equal(detectDataType("datafortestingimport/WebData.expdata"), "web")
+  expect_equal(detectDataType("datafortestingimport/PhysioData.csv"), "physio")
+  expect_equal(detectDataType("datafortestingimport/nonheader.expdata"), "unkown")
+})
+
+test_that("Test of createFileIndex, is index correctly created?", {
+  files <- files <-list.files("datafortestingimport", pattern = "*.csv|.expdata", full.names=T)
+  files <- append(files, c("exp", "exp", "unkown","physio", "web", "web"))
+  test_index <- array(files, dim = c(6,2))
+  expect_equal(createFileIndex("datafortestingimport"), test_index)
+})
