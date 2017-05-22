25e4cdb93e2c3482f9d4e2ea5150382582497c7b
Cocktailrockstar <cocktailrockstar@gmail.com>
2017-05-22 10:46:28 +0200

    correct bug

--- tests/testthat/testimport.R
+++ tests/testthat/testimport.R
@@ -9,8 +9,14 @@ test_that("Detected DataType is real DataType", {
 })
 
 test_that("Test of createFileIndex, is index correctly created?", {
-  files <- files <-list.files("datafortestingimport", pattern = "*.csv|.expdata", full.names=T)
+  files <-list.files("datafortestingimport", pattern = "*.csv|.expdata", full.names=T)
   files <- append(files, c("exp", "exp", "unkown","physio", "web", "web"))
   test_index <- array(files, dim = c(6,2))
+  file_index <- a_files_types[a_files_types[,2] != "unkown",]
   expect_equal(createFileIndex("datafortestingimport"), test_index)
+})
+
+
+test_that("Test of importData",{
+  expect_equal(importData("datafortestingimport", prefix = "TEST"), NULL)
 })
