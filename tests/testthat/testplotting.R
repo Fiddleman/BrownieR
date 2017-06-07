6d35c2b4900981a9b24e0582b0a5c93bc885b8f0
Cocktailrockstar <cocktailrockstar@gmail.com>
2017-06-07 15:40:29 +0200

    create and fix some bugs on tests

--- tests/testthat/testplotting.R
+++ tests/testthat/testplotting.R
@@ -9,3 +9,8 @@ test_that("Test take_screenshot", {
   take_screenshot("https://www.amazon.de/bücher-buch-lesen/b/ref=nav_shopall_bo?ie=UTF8&node=186606", width = 50)
   expect_true(file.exists("screenshots/www.amazon.de-bücher-buch-lesen-b-ref=nav_shopall_bo.png"))
 })
+
+test_that("Test trans_for_webplot", {
+  import("datafortestingimport", prefix = "TEST")
+  expect_equal(names(trans_for_webplot(TEST_web, url = "http://im.iism.kit.edu/home.php", type = "motion", subject = 1)), c("X", "Y", "subject"))
+})
--- tests/testthat/testsummary.R
+++ tests/testthat/testsummary.R
@@ -10,8 +10,7 @@ test_that("Test of summary() WITHOUT objectives", {
 test_that("Test of summary() WITH objectives",{
   import("datafortestingimport", prefix = "TEST")
   objectives <- c(Goals="http://im.iism.kit.edu/")
-  summary_obj_names = c("Label", "URL", "Unique_Conversions", "All_Conversions", "All_Conversion_Rate", "Unique_Conversions_Rate")
-  expect_equal(names(summary(TEST_web, objectives)), summary_obj_names)
+  expect_equal(names(summary(TEST_web, objectives)), c("Label", "URL", "Unique_Conversions", "All_Conversions", "All_Conversion_Rate", "Unique_Conversions_Rate"))
 })
 
 
