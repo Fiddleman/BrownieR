1248201c794894f35dfd36b5e7f3e4fa8940434c
Cocktailrockstar <cocktailrockstar@gmail.com>
2017-06-07 15:40:52 +0200

    nothing change

--- R/globalVar.R
+++ R/globalVar.R
@@ -1,7 +1,6 @@
 ## DataTypes
 # ToDo: Declare Data Types here with identifier in first line of csv., therefore code in DetectDataType must be rewritten!
 
-
 ## Declaration of time colums
 t_cols_physio <-  c("ReceiveTime", "SampleTime")
 t_cols_exp <- c("CLIENT_TIME", "SERVER_TIME")
