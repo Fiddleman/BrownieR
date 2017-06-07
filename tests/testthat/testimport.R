aa2812e7c86ccd1bb9d5e7f76381a8ffef73c134
Cocktailrockstar <cocktailrockstar@gmail.com>
2017-06-07 12:30:29 +0200

    fix bug in file_index test

--- tests/testthat/testimport.R
+++ tests/testthat/testimport.R
@@ -8,12 +8,12 @@ test_that("Detected DataType is real DataType", {
   expect_equal(detectDataType("datafortestingimport/nonheader.expdata"), "unkown")
 })
 
-test_that("Test of createFileIndex, is index correctly created?", {
+test_that("Test of indexFiles, is index correctly created?", {
   files <-list.files("datafortestingimport", pattern = "*.csv|.expdata", full.names=T)
   files <- append(files, c("exp", "exp", "unkown","physio", "web", "web"))
   test_index <- array(files, dim = c(6,2))
-  file_index <- a_files_types[a_files_types[,2] != "unkown",]
-  expect_equal(createFileIndex("datafortestingimport"), test_index)
+  file_index <- test_index[test_index[,2] != "unkown",]
+  expect_equal(indexFiles("datafortestingimport"), file_index)
 })
 
 
