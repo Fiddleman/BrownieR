dfb2094397c2f6d0d3ee54708b80e50d0ee0d575
Cocktailrockstar <cocktailrockstar@gmail.com>
2017-06-07 14:12:27 +0200

    write testcase for take_screenshot

--- tests/testthat/testplotting.R
+++ tests/testthat/testplotting.R
@@ -1,6 +1,11 @@
 library("NeuroIS")
-context("Test of all Screenshot Functions")
+context("Test of all plotting related functions")
 
-test_that("Test url_to_filename Function", {
+test_that("Test url_to_filename", {
   expect_equal(url_to_filename("https://www.amazon.de/bücher-buch-lesen/b/ref=nav_shopall_bo?ie=UTF8&node=186606"), "screenshots/www.amazon.de-bücher-buch-lesen-b-ref=nav_shopall_bo.png")
+})
+
+test_that("Test take_screenshot", {
+  take_screenshot("https://www.amazon.de/bücher-buch-lesen/b/ref=nav_shopall_bo?ie=UTF8&node=186606", width = 50)
+  expect_true(file.exists("screenshots/www.amazon.de-bücher-buch-lesen-b-ref=nav_shopall_bo.png"))
 })
