989d5d452a27e3e40e8c303b4698245785d0cf9b
Cocktailrockstar <cocktailrockstar@gmail.com>
2017-09-01 10:54:05 +0200

    update for moved unisens files

--- R/pyhsio-integration.R
+++ R/pyhsio-integration.R
@@ -9,7 +9,12 @@ initalizeUnisens <-  function(){
   unisensdir <- file.path(system.file("java", package = "NeuroIS"))
   unisensfiles <- c("org.unisens.jar", "org.unisens.ri.jar")
   rJava::.jinit()
-  for(i in unisensfiles){
-    rJava::.jaddClassPath(file.path(unisensdir,i))
-  }
+  # Old implementation, but unisens files are now bind to package
+  # for(i in unisensfiles){
+  #   rJava::.jaddClassPath(file.path(unisensdir,i))
+  # }
+  
+  # new implementation with system.file
+  rJava::.jaddClassPath(system.file("java", "org.unisens.jar", package = "NeuroIS", mustWork = T))
+  rJava::.jaddClassPath(system.file("java", "org.unisens.ri.jar", package = "NeuroIS", mustWork = T))
 }
