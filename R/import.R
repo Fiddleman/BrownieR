e8db7c048d40b8a8aae092593820a45d0d244384
Cocktailrockstar <cocktailrockstar@gmail.com>
2017-10-22 16:52:18 +0200

    fix bug in indexFiles() that occure if folder contains only one file

--- R/import.R
+++ R/import.R
@@ -1,15 +1,15 @@
 #' import(path, prefix)
 #' 
 #' Imports brownie data (of heterogenous types) from folder and adds list containing dataframes and type to global environment (nameing by prefix).
-#' It only imports data of class web and exp directly in R, due to the fact, that pyhsio-data is too big, it moves it in 
-#' a subdirectory, in *path*/physio/
 #' 
 #' @param path; a character
 #' @param prefix; a character, nameing of lists
+#' @param physio_mv_dir; a logical, indicates if physio data should be moved in subfolder "physio", necessary for create unisense data objects
+#' @param create_physio_object; a logical, indicates if physio data should be added to enviornment, necessary for do eda summary and eda plot
 #' @return none (adds list directly to global environment)
 #' @export
 
-import <- function(path, prefix){
+import <- function(path, prefix, physio_mv_dir = T, create_physio_object = F){
   file_index <- indexFiles(path = path)
   datatypes <- unique(file_index[,2])
   for (dtype in datatypes) {
@@ -20,13 +20,29 @@ import <- function(path, prefix){
       data_object <- structure(data, class = dtype, files = files)
       assign(paste0(prefix, "_", dtype), data_object, envir = .GlobalEnv)
     } else {
-      #move all physio files to folder ./physio/data
-      topath <- paste0(path,file.path("/physio/"))
-      files <- file_index[file_index[,2] == dtype, 1]
-      if (!dir.exists(topath)) dir.create(topath, recursive=TRUE)
-      for (file in files) {
-        tofilename = paste0(topath, basename(file))
-        file.rename(from = file, to = tofilename)      
+      if (create_physio_object){
+        files <- file_index[file_index[,2] == "physio", 1]
+        count <- 1
+        data <- list()
+        for (file in files) {
+          single_df <- read.csv(file = file, sep = ";", header = T, stringsAsFactors = F)
+          single_df <- rm_empty_col(single_df)
+          single_df <- convertTime(single_df, dtype)
+          data[[count]] <- single_df
+          count <- count + 1
+        }
+      data_object <- structure(data, class = dtype, files = files)
+      assign(paste0(prefix, "_", dtype), data_object, envir = .GlobalEnv)
+      if (physio_mv_dir){
+        #move all physio files to folder ./physio/data
+        topath <- paste0(path, file.path("/physio/"))
+        files <- file_index[file_index[,2] == dtype, 1]
+        if (!dir.exists(topath)) dir.create(topath, recursive=TRUE)
+        for (file in files) {
+          tofilename = paste0(topath, basename(file))
+          file.rename(from = file, to = tofilename)            
+        }
+      }
       }
     }
   }
@@ -112,3 +128,25 @@ convertTime <- function(data, type){
     } else stop("Filetype not supported, only web, physio or exp is possible.")
   return(data)
 }
+
+#' rm_empty_col()
+#' 
+#' Removes every colum containing zeros, as it appears for inputs with no connected sensor in a experiment.
+#' This reduces the size to a half.
+#' 
+#' @param data a physio dataframe 
+#' @return dataframe
+
+rm_empty_col <- function(data) {
+  colums <- 1:length(data)
+  del_col <- NULL
+  for (colum in colums) {
+    if (all(data[colum] == 0)) del_col <- c(del_col, colum)
+  }
+  if (!is.null(del_col)) data <- data[,-del_col]
+  return(data)
+}
+
+
+
+
