838172b38325ff7be4bc5ea4ef1de349380bf09c
Cocktailrockstar <cocktailrockstar@gmail.com>
2017-10-23 09:49:28 +0200

    add write file option

--- R/physio-marker.R
+++ R/physio-marker.R
@@ -4,11 +4,12 @@
 #' For unisens, this file needs to be in *wd*/*data*/physio .
 #' 
 #' @param data a list of class exp or web
-#' @param subject, a integer identifies subject for which marker file will be created
+#' @param subject, a integer identifies subject for which marker file shall be created
+#' @param write_marker_file, a logical; it's necessary to create a marker file to file system for potting ecg data.
 #' @export
 #' @return a dataframe 
 
-marker <- function(data, subject){
+marker <- function(data, subject, write_marker_file = F){
   if (class(data) == "exp"){
     data <- as.data.frame(unclass(data), stringsAsFactors = F)
     data <- subset(data,SUBJECT_ID_SUBJECT == subject, select = c(CLIENT_TIME, SCREEN_NAME)) #filter by specified subject, keep only ClienTime, Screen    
@@ -22,5 +23,18 @@ marker <- function(data, subject){
   data[ ,1] <- as.numeric(data[ ,1]) - as.numeric(min(data[ ,1], na.rm = T)) #calculate duration
   names(data) <- c("DURATION", "0", "EVENT")
   data <- data[order(data$DURATION),] #order by duration
+  if (write_marker_file){
+    file <- paste0("marker", subject, ".csv")
+    filename <- file.path("data/pyhsio", file)
+    filename <- "data/physio/marker174.csv"
+    write.table(data, 
+                file = filename,
+                row.names=FALSE,
+                na="",
+                col.names=FALSE,
+                sep=";",
+                quote = FALSE)
+  }
+ 
   return(data)
 }
